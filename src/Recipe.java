d32bf7755ef276407febd0ed001672f8e4a14032
dpennebacker <dpennebacker@pvsd.org>
2016-10-11 09:42:45 -0400

    Initial commit

---
+++ src/Recipe.java
@@ -0,0 +1,42 @@
+/**
+ * Created by dpennebacker on 10/11/16.
+ */
+public class Recipe {
+
+        public Recipe(String theName)
+        {
+            //something
+        }
+
+    public void setServings(int x)
+    {
+        //more stuff
+    }
+
+public double getRetailCost()
+{
+    //epic <code></code>
+    int x = 13;
+    double tempCost = pricePerCalorie(x) * calories + cost;
+    return tempCost;
+
+}
+    private double pricePerCalorie(int z)
+    {
+        //more stuff
+    }
+
+    public int calories;
+    public int carbs;
+    public int fat;
+    private double cost;
+    public String name;
+
+
+
+
+
+
+
+
+}
